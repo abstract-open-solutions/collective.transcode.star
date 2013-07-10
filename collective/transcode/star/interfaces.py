648d8759a0e5dfd652a7e43e0451f3041cc8427f
Simone Orsi <simahawk@gmail.com>
2013-07-10 13:02:38 +0200

    PEP8 cleanup

--- collective/transcode/star/interfaces.py
+++ collective/transcode/star/interfaces.py
@@ -1,65 +1,81 @@
+#-*- coding: utf-8 -*-
+
 """The product's interfaces"""
 from zope import schema
 from zope.interface import Interface
 from zope.component.interfaces import IObjectEvent
 
+
 class ITranscodedEvent(IObjectEvent):
-    profile = schema.TextLine(title = u"Transcode profile")
+
+    profile = schema.TextLine(title=u"Transcode profile")
 
 
 #TODO: add help messages
 class ITranscodeSettings(Interface):
     """Transcoding settings"""
-    daemon_address = schema.Tuple(title = u'Daemon address',
-                                  value_type = schema.TextLine(title = u'address'),
-                                  default = (u'http://localhost:8888',),
-                                  )
 
-    transcode_profiles = schema.Tuple(title = u'Transcode profiles',
-                                      value_type = schema.TextLine(title = u''),
-                                      default = (u'jpeg',
-                                                 u'webm-low',                                                 
-                                                 u'mp4-low',
-                                                 u'mp4-high',
-                                                 u'webm-high',
-                                                ),
-                                      )
+    daemon_address = schema.Tuple(
+        title=u'Daemon address',
+        value_type=schema.TextLine(title=u'address'),
+        default=(u'http://localhost:8888',),
+    )
 
-    portal_types = schema.Tuple(title = u'Portal types to transcode',
-                                value_type = schema.TextLine(title = u''),
-                                default = (u'File',),
-                                )
+    transcode_profiles = schema.Tuple(
+        title=u'Transcode profiles',
+        value_type=schema.TextLine(title=u''),
+        default=(
+            u'jpeg',
+            u'webm-low',
+            u'mp4-low',
+            u'mp4-high',
+            u'webm-high',
+        ),
+    )
 
-    mime_types = schema.Tuple(title = u'Supported mime types',
-                              value_type = schema.TextLine(title = u''),
-                              default = ('application/ogg',
-                                        'video/ogg',
-                                        'video/x-ogg',
-                                        'video/x-ogm+ogg',
-                                        'video/flv',
-                                        'video/x-flv',
-                                        'video/mpeg',
-                                        'video/3gpp',
-                                        'video/x-ms-wmv',
-                                        'video/quicktime',
-                                        'video/x-la-asf',
-                                        'video/x-ms-asf',
-                                        'video/x-msvideo',
-                                        'video/mp4',
-                                        'video/webm',),
-                              )
+    portal_types = schema.Tuple(
+        title=u'Portal types to transcode',
+        value_type=schema.TextLine(title=u''),
+        default=(u'File',),
+    )
 
-    secret = schema.TextLine(title = u'Shared secret with transcode daemon(s)',
-                             default = u'1771d99931264d538e75eeb19da7d6a0',
-                            )
+    mime_types = schema.Tuple(
+        title=u'Supported mime types',
+        value_type=schema.TextLine(title=u''),
+        default=(
+            'application/ogg',
+            'video/ogg',
+            'video/x-ogg',
+            'video/x-ogm+ogg',
+            'video/flv',
+            'video/x-flv',
+            'video/mpeg',
+            'video/3gpp',
+            'video/x-ms-wmv',
+            'video/quicktime',
+            'video/x-la-asf',
+            'video/x-ms-asf',
+            'video/x-msvideo',
+            'video/mp4',
+            'video/webm',
+        ),
+    )
 
-    subtitles = schema.Bool(title = u'Show subtitles',
-                             default = True,
-                            )
+    secret = schema.TextLine(
+        title=u'Shared secret with transcode daemon(s)',
+        default=u'1771d99931264d538e75eeb19da7d6a0',
+    )
 
-    showDownload = schema.Bool(title = u'Show download button to embedded clips',
-                             default = True,
-                            )
+    subtitles = schema.Bool(
+        title=u'Show subtitles',
+        default=True,
+    )
+
+    showDownload = schema.Bool(
+        title=u'Show download button to embedded clips',
+        default=True,
+    )
+
 
 class ITranscodeTool(Interface):
     """TranscodeTool interface"""
@@ -67,7 +83,7 @@ class ITranscodeTool(Interface):
     def add(self, obj):
         """add object to transcoding queue if not already there"""
 
-    def status(self, obj, profile, fieldName = None):
+    def status(self, obj, profile, fieldName=None):
         """Check if it is transcoded with given profile"""
 
     def callback(self, context, request):
@@ -76,11 +92,13 @@ class ITranscodeTool(Interface):
     def errback(self, context, request):
         """handle errbacks"""
 
+
 class ICallbackView(Interface):
     """CallbackView interface"""
 
     def callback_xmlrpc(self, result):
         """callback"""
+
 
 class ITranscoded(Interface):
     """Marker interface for transcoded content"""
