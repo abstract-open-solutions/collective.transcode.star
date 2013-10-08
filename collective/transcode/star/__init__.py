76f6dfec09c2b69de1c6e451a682d9e14054e35b
Simone Orsi <simahawk@gmail.com>
2013-10-08 12:27:47 +0200

    add message factory

--- collective/transcode/star/__init__.py
+++ collective/transcode/star/__init__.py
@@ -1,4 +1,11 @@
-  # -*- extra stuff goes here -*- 
+  # -*- extra stuff goes here -*-
+
+from zope.i18nmessageid import MessageFactory
+
+# Set up the i18n message factory for our package
+messageFactory = MessageFactory('collective.transcode.star')
+_ = messageFactory
+
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
